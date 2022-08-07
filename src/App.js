bd4fe85ff190591edda7783477892faa020a41e1
JayanaGunaweera01 <jayana.20200003@iit.ac.lk>
2022-08-07 15:25:01 +0530

    Updated App.js with view cerificate and download

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import ImgCarousel from './components/carousel/ImgCarousel';
+import Certificates from './components/certificate/Certificates'
 import Footer from './components/footer/Footer';
 import Hero from './components/hero/Hero'
 import Navbar from './components/navbar/Navbar'
 import Search from './components/search/Search';
 import Selects from './components/selects/Selects';
+
 
 function App() {
   return (
@@ -14,6 +16,7 @@ function App() {
       <Search />
       <Selects />
       <ImgCarousel />
+      <Certificates />
       <Footer />
     </div>
   );
---
+++ src/components/certificate/Certificates.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './CertificatesStyles.css'
+
+import Certificate from '../../assets/certificate.jpg'
+
+
+function Certificates() {
+    return (
+        <div name='certificates' className='certificates'>
+            <div className="container">
+                <h1>Download your certificate here</h1>
+                
+                <div className="img-container">
+                    <img className='span-3 image-grid-row-2' src={Certificate} alt="/"/>
+                </div>
+                <button>Download Police Certificate </button>
+            </div>
+          
+        </div>
+    )
+}
+
+export default Certificates
