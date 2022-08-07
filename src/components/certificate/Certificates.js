35c3c89362850507243cfa41288395618c15d15e
JayanaGunaweera01 <jayana.20200003@iit.ac.lk>
2022-08-07 19:43:21 +0530

    Added a frame and changed size of certificate

--- src/components/certificate/Certificates.js
+++ src/components/certificate/Certificates.js
@@ -1,23 +1,21 @@
-import React from 'react'
-import './CertificatesStyles.css'
+import React from "react";
+import "./CertificatesStyles.css";
 
-import Certificate from '../../assets/certificate.jpg'
-
+import Certificate from "../../assets/certificate.jpg";
 
 function Certificates() {
-    return (
-        <div name='certificates' className='certificates'>
-            <div className="container">
-                <h1>Download your certificate here</h1>
-                
-                <div className="img-container">
-                    <img className='span-3 image-grid-row-2' src={Certificate} alt="/"/>
-                </div>
-                <button>Download Police Certificate </button>
-            </div>
-          
+  return (
+    <div name="certificates" className="certificates">
+      <div className="container">
+        <h1>Download your certificate here</h1>
+
+        <div className="img-container">
+          <img className="span-3 image-grid-row-2" src={Certificate} alt="/" />
         </div>
-    )
+        <button>Download Police Certificate </button>
+      </div>
+    </div>
+  );
 }
 
-export default Certificates
+export default Certificates;
