bbed7ecc587fe80584560f5fccb1a89d80379cf4
areebniyas <areebniyas@gmail.com>
2022-08-07 11:27:00 +0530

    Added ID Tokens

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -12,71 +12,81 @@ import './NavbarStyles.css'
 function Navbar() {
     const [nav, setNav] = useState(false)
     const handleNav = () => setNav(!nav)
-    const { state, signIn, signOut, getAccessToken } = useAuthContext();
+    const { state, signIn, signOut, getAccessToken, getIDToken } = useAuthContext();
     const obtainAccessToken = () => {
         getAccessToken().then((accessToken) => {
-           console.log(accessToken);
-       }
-   )};
-    
+            console.log(accessToken);
+        }
+        )
+    };
+
+    const obtainIDToken = () => {
+        getIDToken().then((IDToken) => {
+            console.log(IDToken);
+        }
+        )
+    };
+
     return (
         <div name='home' className={nav ? 'navbar navbar-bg' : 'navbar'}>
-        <div className={nav ? 'logo dark' : 'logo'}>
-        <h2>Your police report is just one click away!</h2>
-        </div>
-        <ul className="nav-menu">
-        <Link to='home' smooth={true} duration={500} ><li>Home</li></Link>
-        <Link to='search' smooth={true} duration={500} ><li>Check status</li></Link>
-        <Link to='search' smooth={true} duration={500} ><li>Request a certificate</li></Link>
-        <Link to='Help' smooth={true} duration={500} ><li>Help</li></Link>
-        <div>
-        {
-            state.isAuthenticated
-            ? (
-                <div>
-                <li><button onClick={() => signOut()}>Logout</button></li>
-                </div>
-                )
-                : <li><button onClick={() => signIn()}>Login</button></li>
-            }
+            <div className={nav ? 'logo dark' : 'logo'}>
+                <h2>Your police report is just one click away!</h2>
             </div>
-            <li><button onClick={() => obtainAccessToken()}>Get Token</button></li>
+            <ul className="nav-menu">
+                <Link to='home' smooth={true} duration={500} ><li>Home</li></Link>
+                <Link to='search' smooth={true} duration={500} ><li>Check status</li></Link>
+                <Link to='search' smooth={true} duration={500} ><li>Request a certificate</li></Link>
+                <Link to='Help' smooth={true} duration={500} ><li>Help</li></Link>
+                <div>
+                    {
+                        state.isAuthenticated
+                            ? (
+                                <div>
+                                    <li><button onClick={() => signOut()}>Logout</button></li>
+                                    <li><button onClick={() => obtainAccessToken()}>Get Access Token</button></li>
+                                    <li><button onClick={() => obtainIDToken()}>Get ID Token</button></li>
+                                </div>
+                            )
+                            : <li><button onClick={() => signIn()}>Login</button></li>
+                    }
+                </div>
+
             </ul>
             <div className="nav-icons">
-            <BiSearch className='icon' style={{ marginRight: '1rem' }} />
-            <BsPerson className='icon' />
+                <BiSearch className='icon' style={{ marginRight: '1rem' }} />
+                <BsPerson className='icon' />
             </div>
             <div className="hamburger" onClick={handleNav}>
-            {!nav ? (<HiOutlineMenuAlt4 className='icon' />) : (<AiOutlineClose style={{ color: '#000' }} className='icon' />)}
-            
-            </div>
-            
-            <div className={nav ? 'mobile-menu active' : 'mobile-menu'}>
-            <ul className="mobile-nav">
-            <Link to='home' smooth={true} duration={500} ><li>Home</li></Link>
-            <Link to='destinations' smooth={true} duration={500} ><li>Service catalog</li></Link>
-            <Link to='carousel' smooth={true} duration={500} ><li>Travel</li></Link>
-            <Link to='search' smooth={true} duration={500} ><li>Book</li></Link>
-            <Link to='views' smooth={true} duration={500} ><li>Views</li></Link>
-            </ul>
-            <div className="mobile-menu-bottom">
-            <div className="menu-icons">
-            <button>Search</button>
-            <button>Account</button>
-            </div>
-            <div className="social-icons">
-            <FaFacebook className='icon' />
-            <FaInstagram className='icon' />
-            <FaTwitter className='icon' />
-            <FaPinterest className='icon' />
-            <FaYoutube className='icon' />
-            </div>
-            </div>
-            </div>
-            
-            </div>
-            
-            )
-        }
+                {!nav ? (<HiOutlineMenuAlt4 className='icon' />) : (<AiOutlineClose style={{ color: '#000' }} className='icon' />)}
 
-        export default Navbar
+            </div>
+
+            <div className={nav ? 'mobile-menu active' : 'mobile-menu'}>
+                <ul className="mobile-nav">
+                    <Link to='home' smooth={true} duration={500} ><li>Home</li></Link>
+                    <Link to='destinations' smooth={true} duration={500} ><li>Service catalog</li></Link>
+                    <Link to='carousel' smooth={true} duration={500} ><li>Travel</li></Link>
+                    <Link to='search' smooth={true} duration={500} ><li>Book</li></Link>
+                    <Link to='views' smooth={true} duration={500} ><li>Views</li></Link>
+                </ul>
+                <div className="mobile-menu-bottom">
+                    <div className="menu-icons">
+                        <button>Search</button>
+                        <button>Account</button>
+                    </div>
+                    <div className="social-icons">
+                        <FaFacebook className='icon' />
+                        <FaInstagram className='icon' />
+                        <FaTwitter className='icon' />
+                        <FaPinterest className='icon' />
+                        <FaYoutube className='icon' />
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    )
+}
+
+export default Navbar
